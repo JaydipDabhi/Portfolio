37bbc6c925ef2e70fc543b9e489057bb489f532a
JaydipDabhi <jaydipdabhi01@gmail.com>
2024-12-19 23:02:54 +0530

    enqueue style and script

--- wp-content/themes/portfoilo/functions.php
+++ wp-content/themes/portfoilo/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Portfoilo functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package Portfoilo
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function portfoilo_setup() {
+function portfoilo_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on Portfoilo, use a find and replace
 		* to change 'portfoilo' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'portfoilo', get_template_directory() . '/languages' );
+	load_theme_textdomain('portfoilo', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function portfoilo_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'portfoilo' ),
+			'menu-1' => esc_html__('Primary', 'portfoilo'),
 		)
 	);
 
@@ -83,7 +85,7 @@ function portfoilo_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +102,7 @@ function portfoilo_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'portfoilo_setup' );
+add_action('after_setup_theme', 'portfoilo_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'portfoilo_setup' );
  *
  * @global int $content_width
  */
-function portfoilo_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'portfoilo_content_width', 640 );
+function portfoilo_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('portfoilo_content_width', 640);
 }
-add_action( 'after_setup_theme', 'portfoilo_content_width', 0 );
+add_action('after_setup_theme', 'portfoilo_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function portfoilo_widgets_init() {
+function portfoilo_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'portfoilo' ),
+			'name'          => esc_html__('Sidebar', 'portfoilo'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'portfoilo' ),
+			'description'   => esc_html__('Add widgets here.', 'portfoilo'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +136,23 @@ function portfoilo_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'portfoilo_widgets_init' );
+add_action('widgets_init', 'portfoilo_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function portfoilo_scripts() {
-	wp_enqueue_style( 'portfoilo-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'portfoilo-style', 'rtl', 'replace' );
+function portfoilo_scripts()
+{
+	wp_enqueue_style('portfoilo-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('portfoilo-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'portfoilo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('portfoilo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'portfoilo_scripts' );
+add_action('wp_enqueue_scripts', 'portfoilo_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +177,32 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function custom_enqueue_styles_and_scripts()
+{
+	// Enqueue Styles
+	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/vendor/bootstrap/css/bootstrap.min.css');
+	wp_enqueue_style('bootstrap-icons', get_template_directory_uri() . '/assets/vendor/bootstrap-icons/bootstrap-icons.css');
+	wp_enqueue_style('aos', get_template_directory_uri() . '/assets/vendor/aos/aos.css');
+	wp_enqueue_style('glightbox-min', get_template_directory_uri() . '/assets/vendor/glightbox/css/glightbox.min.css');
+	wp_enqueue_style('swiper-bundle-min', get_template_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.css');
+	wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css');
+
+	// Enqueue Scripts
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('bootstrap-bundle-min', get_template_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array('jquery'), null, true);
+	wp_enqueue_script('validate', get_template_directory_uri() . '/assets/vendor/php-email-form/validate.js', array(), null, true);
+	wp_enqueue_script('aos', get_template_directory_uri() . '/assets/vendor/aos/aos.js', array(), null, true);
+	wp_enqueue_script('typed-umd', get_template_directory_uri() . '/assets/vendor/typed.js/typed.umd.js', array(), null, true);
+	wp_enqueue_script('purecounter-vanilla', get_template_directory_uri() . '/assets/vendor/purecounter/purecounter_vanilla.js', array(), null, true);
+	wp_enqueue_script('noframework-waypoints', get_template_directory_uri() . '/assets/vendor/waypoints/noframework.waypoints.js', array(), null, true);
+	wp_enqueue_script('glightbox-min', get_template_directory_uri() . '/assets/vendor/glightbox/js/glightbox.min.js', array(), null, true);
+	wp_enqueue_script('imagesloaded-pkgd-min', get_template_directory_uri() . '/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js', array(), null, true);
+	wp_enqueue_script('isotope-pkgd-min', get_template_directory_uri() . '/assets/vendor/isotope-layout/isotope.pkgd.min.js', array(), null, true);
+	wp_enqueue_script('swiper-bundle-min', get_template_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.js', array(), null, true);
+	wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'custom_enqueue_styles_and_scripts');
